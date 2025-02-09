e25be95eda28180ce49440542e17680f5582613c
Alejandro <alejandro.ginesmartinez@gmail.com>
2025-02-09 19:43:40 +0100

    create retriever

---
+++ src/retriever.ts
@@ -0,0 +1,33 @@
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeStore } from "@langchain/pinecone";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
+const PINECONE_INDEX = process.env.PINECONE_INDEX;
+const EMBEDDING_LLM_MODEL = process.env.EMBEDDING_LLM_MODEL;
+
+if (!PINECONE_API_KEY) {
+    throw new Error("Missing Pinecone API. Check your .env file.");
+}
+
+if (!PINECONE_INDEX) {
+    throw new Error("Missing Pinecone key Index. Check your. env file. ");
+}
+
+if (!EMBEDDING_LLM_MODEL) {
+    throw new Error("Missing Embedding LLM model. Check your. env file. ");
+}
+
+const pinecone = new Pinecone({ apiKey: PINECONE_API_KEY });
+const pineconeIndex = pinecone.Index(PINECONE_INDEX);
+
+export async function createRetriever() {
+    const embeddingLLM = new OpenAIEmbeddings({model: EMBEDDING_LLM_MODEL});
+    
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddingLLM, {pineconeIndex});
+
+    return vectorStore.asRetriever();
+}
